bce810e0d887e025248e4fd361f17e628d469a01
Daniel Yates <me@meorawr.io>
2021-03-29 01:33:19 +0100

    Fix improper callback registration

--- totalRP3/modules/NamePlates/NamePlates_Blizzard.lua
+++ totalRP3/modules/NamePlates/NamePlates_Blizzard.lua
@@ -155,7 +155,7 @@ function TRP3_BlizzardNamePlates:OnModuleEnable()
 
 	self.initializedNameplates = {};
 
-	TRP3.NamePlates.RegisterCallback(self, "OnUnitDisplayInfoUpdated");
+	TRP3.NamePlates.RegisterCallback("OnUnitDisplayInfoUpdated", self.OnUnitDisplayInfoUpdated, self);
 
 	hooksecurefunc("CompactUnitFrame_SetUpFrame", function(...) return self:OnUnitFrameSetUp(...); end);
 	hooksecurefunc(NamePlateDriverFrame, "UpdateNamePlateOptions", function() return self:OnUpdateNamePlateOptions(); end);
